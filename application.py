31fec0dcff3240e4c736f9e454aad13eb8c55600
AASTHA SOLANKI <aasthasolanki1705@gmail.com>
2026-09-06 11:25:55 +0530

    Alias joblib as jb in application.py

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 import pandas as pd
-import joblib
+import joblib as jb
 
 # Load the saved linear regression model
 loaded_lr_model = joblib.load('linear_regress_model.sav')
